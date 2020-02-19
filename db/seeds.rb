50b91516150ee2ffa2a2a8bc7dacdfbcc876b18b
Shane Wickramasuriya <shane.geeky@gmail.com>
2020-02-19 14:53:36 +0000

    Completed with forms in restaurants

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Creating restaurants"
+
+category = %w(chinese italian japanese french belgian)
+
+10.times do
+  restaurant = Restaurant.create(
+    name: Faker::Movies::HarryPotter.spell,
+    address: Faker::Movies::HarryPotter.location,
+    phone_number: Faker::PhoneNumber.cell_phone,
+    category: category.sample
+  )
+  puts ".........."
+end
+
+puts "Done"
